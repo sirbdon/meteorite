e6043317210ff92f8cb0974611c21143369cf47d
Brandon Pizzacalla <bpizzacalla@gmail.com>
2016-02-24 11:04:33 -0800

    Wired collection to postList template

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,18 +1,5 @@
-var postData = [
-	{
-		title: "Brandon's Meteor App",
-		url: "http://google.ca"
-	},
-	{
-		title: "fuck shit",
-		url: "http://dontgohere.com"
-	},
-	{
-		title: "A third one",
-		url: "http://www.fake.com/fakeme"
-	}
-];
-
 Template.postsList.helpers({
-	posts: postData
+	posts: function() {
+		return Posts.find();
+	}
 });
