804b62576e1750e182fc28052ae4c5dadedbb25d
Brandon Pizzacalla <bpizzacalla@gmail.com>
2016-02-29 12:31:34 -0800

    created duplicateUrlCheck method for submit and update posts; deny post update if URL exists

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -8,6 +8,26 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
+    
+    // CHECK FOR EXISTING URL ON EDIT
+    var duplicateUrlResult = {};
+
+    Meteor.call('duplicateUrlCheck', postProperties.url, function(error, result) {
+      // if error return error to console
+      if (error) return console.log(error);
+      duplicateUrlResult = result;
+    })
+
+    // if url is duplicate show post exists and route to existing page
+    // do not update post (remains the same)
+    if (duplicateUrlResult.postExists) {
+      alert('Ya that URL exists already idiot');
+      console.log(result.postExists);
+      Router.go('postPage', {_id: result._id});
+      return;
+    }
+
+    // ELSE, UPDATE POST PROPERTIES
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
       // display the error to the user
